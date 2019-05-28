4fb5fa71c4fb3cfd7b7f16f4cd2e95baad635811
Alexander Morris <qd01a@yourika.com>
2019-05-28 07:01:16 -0400

    Create whaleshares_v2_settings.js

---
+++ whaleshares_v2_settings.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Auto-Enable Whaleshares v2 Slider
+// @namespace    https://whaleshares.io/@alexpmorris
+// @version      0.01
+// @description  Protects MANA against a disabled slider setting
+// @author       @alexpmorris
+// @match        https://whaleshares.io/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var settings = localStorage.getItem('settings');
+    if (settings == null) {
+      // set default settings and enable slider at 25%
+      localStorage.setItem('settings', "{\"locale\":\"en-US\",\"votingPower\":\"on\",\"votePercent\":2500,\"showNSFWPosts\":true,\"rewriteLinks\":false}");
+      alert("Script: Set Default Whaleshares Settings and Enabled Slider!");
+    }
+
+})();
