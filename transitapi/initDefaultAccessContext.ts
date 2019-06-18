2361782ff1ea2f5d743f0c5fd64ad46fd613b199
Alexander Morris <qd01a@yourika.com>
2019-06-18 11:36:29 -0400

    Create initDefaultAccessContext.ts

---
+++ transitapi/initDefaultAccessContext.ts
@@ -0,0 +1,31 @@
+import { initDefaultAccessContext } from 'eos-transit';
+import myWallet from './mywallet';
+
+const appName = 'transit-dev-whalevault';
+
+const walContext = initDefaultAccessContext({
+	appName,
+	network: {
+		host: 'api.eosnewyork.io',
+		port: 80,
+		protocol: 'http',
+		chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+	},
+	walletProviders: [ myWallet() ]
+});
+
+declare global {
+    interface Window { walContext: any; }
+}
+window.walContext = walContext;
+
+// const walContext = initDefaultAccessContext({
+// 	appName,
+// 	network: {
+// 		host: 'api.pennstation.eosnewyork.io',
+// 		port: 7101,
+// 		protocol: 'https',
+// 		chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
+// 	},
+// 	walletProviders: [ myWallet() ]
+// });
